08b468af2cd6b63ccc1ca78e3238c1413435575d
Tsengwen <benson326789@gmail.com>
2020-12-01 14:08:35 +0800

    Add gcin install

--- setup.sh
+++ setup.sh
@@ -20,7 +20,10 @@ sudo apt upgrade -y
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - 
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list 
 sudo apt update
-sudo apt install sublime-text
+sudo apt install sublime-text -y
+sudo apt-get install gcin gcin-qt5-immodule gcin-anthy gcin-voice -y
+im-config -n gcin
+
 
 MYPWD=$(pwd)
 
